fb8bd30d4f8f3eb2f64b52b2887304d1127eea4d
hassan <hassan@gmail.com>
2026-03-09 14:21:23 +0530

    add files

--- Dockerfile
+++ Dockerfile
@@ -17,7 +17,7 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
 # FIX: Run collectstatic to generate the folder WhiteNoise needs
-RUN python manage.py collectstatic --noinput
+RUN python manage.py collectstatic --noinput --clear --skip-checks
 RUN ls -R /app/staticfiles/drf-yasg/
 
 EXPOSE 8000
--- breathline/settings.py
+++ breathline/settings.py
@@ -1,16 +1,14 @@
 import os, dj_database_url, ast
 from pathlib import Path
-from dotenv import load_dotenv
 
-load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# --- SECRETS & BUILD COMPATIBILITY ---
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-build-placeholder')
+# --- Security ---
+SECRET_KEY = os.environ.get('SECRET_KEY', 'build-key-123')
 DEBUG = ast.literal_eval(os.environ.get('DEBUG', 'False'))
-ALLOWED_HOSTS = ["*"] 
+ALLOWED_HOSTS = ["*"]
 
-# --- APPLICATION DEFINITION ---
+# --- Apps ---
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -18,21 +16,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Third Party
     'rest_framework',
     'drf_yasg',
     'corsheaders',
-    'django_acl',  # <--- Ensure this is the correct package name
-    # Local Apps
+    'django_acl', # MUST BE HERE
     'apps.user',
     'apps.authentication',
     'apps.home',
     'apps.blog',
 ]
 
+# --- WhiteNoise & Middleware ---
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # Position is vital
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Fixed 404/MIME
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -42,32 +39,26 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'breathline.urls'
-WSGI_APPLICATION = 'breathline.wsgi.application'
-
-# --- DATABASE ---
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'postgres://u:p@localhost:5432/db'),
-        conn_max_age=600,
-    )
-}
-
-# --- STATIC FILES (Stop 404s/MIME errors) ---
+# --- Static Files Fix ---
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-# --- HTTP-ONLY SECURITY (Fix "Untrustworthy Origin") ---
+# --- Untrusted Origin (HTTP) Fixes ---
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = False
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 
-# --- SWAGGER ---
-SWAGGER_SETTINGS = {
-    'USE_SESSION_AUTH': False,
-    'VALIDATOR_URL': None,
+ROOT_URLCONF = 'breathline.urls'
+WSGI_APPLICATION = 'breathline.wsgi.application'
+AUTH_USER_MODEL = 'user.Users'
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600,
+    )
 }
