949f666415513af725a2353d5acbbcf0b93c6a0c
root <root@ip-172-31-33-30.ap-south-1.compute.internal>
2026-03-12 06:48:05 +0000

    add the changes

--- scripts/start_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/django-api/
-# Pull the latest image we just pushed in CodeBuild
-docker-compose pull
-# Restart the containers with the new image
-docker-compose up -d --remove-orphans
